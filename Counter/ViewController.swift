b7175438001723972d1578dacb276ea5b62e7bc7
Sayan B <sayan.badarchy@gmail.com>
2023-11-20 23:09:49 +0400

    add history log
    refactoring

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
+enum CounterAction {
+    case reset
+    case increase
+    case decrease
+}
+
 class ViewController: UIViewController {
 
-    private var _counter: Int = 0 {
-        didSet {
-            counterLabel.text = _counter > 0 ? "Значение счётчика: \(_counter)" : "0"
-        }
-    }
+    private var _counter: Int = 0
     var counter: Int {
         get { return _counter }
         set {
@@ -22,21 +24,49 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func increaseCounter(_ sender: Any) {
-        counter += 1
+        changeCounter(action: .increase)
     }
     
     @IBAction func decreaseCounter(_ sender: Any) {
-        counter -= 1
+        changeCounter(action: .decrease)
     }
     
     @IBAction func resetCounter(_ sender: Any) {
-        counter = 0
+        changeCounter(action: .reset)
+    }
+    
+    func changeCounter(action: CounterAction) {
+        var logEntry: String
+        
+        switch action {
+        case .decrease:
+            logEntry = counter - 1 < 0 ? "попытка уменьшить значение счётчика ниже 0" : "значение изменено на -1"
+            counter = counter - 1 < 0 ? 0 : counter - 1
+        case .increase:
+            logEntry = "значение изменено на +1"
+            counter += 1
+        case .reset:
+            logEntry = "значение сброшено"
+            counter = 0
+        }
+        updateCounterText()
+        logNewEntry(logEntry)
+    }
+    
+    func updateCounterText() {
+        counterLabel.text = counter > 0 ? "Значение счётчика: \(counter)" : "0"
+    }
+    
+    func logNewEntry(_ entry: String) {
+        let now = Date()
+        historyTextView.text += "[\(now.formatted(date: .numeric, time: .shortened))]: \(entry)\n"
     }
 }
 
