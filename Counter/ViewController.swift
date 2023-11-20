603689f73bec38c3ace92fd104c6b3ff51346ff5
Sayan B <sayan.badarchy@gmail.com>
2023-11-20 22:09:56 +0400

    Sprint № 3
    add label to hold value of the counter
    add two buttons: increase and deacrease the counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var _counter: Int = 0 {
+        didSet {
+            counterLabel.text = "Значение счётчика: \(_counter)"
+        }
+    }
+    var counter: Int {
+        get { return _counter }
+        set {
+            _counter = max(newValue, 0)
+        }
+    }
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func increaseCounter(_ sender: Any) {
+        counter += 1
+    }
+    
+    @IBAction func decreaseCounter(_ sender: Any) {
+        counter -= 1
+    }
+    
 }
 
